753b66528fe675070deeafd92e6ea1fdf25341b5
bengunur06 <bengunur2@gmail.com>
2020-08-31 17:27:13 +0300

    examples

---
+++ ex1.py
@@ -0,0 +1,5 @@
+# - *- coding: utf- 8 - *-
+print "Hello World! "
+#print " KEdiler gunü"
+print "this iss funnn"
+print "Bengünur Baş"
---
+++ operationses3.py
@@ -0,0 +1,15 @@
+# - *- coding: utf- 8 - *-
+print "Now i will count my chicken "
+
+print "Ckicken ", 20 + 20 / 6 
+print "roosters ", 100 - 25 * 3 %4
+
+print " Now i will count my egg"
+print 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6 
+
+print "Is  it true that 3+2 < 5 - 7"
+print  3 + 2 < 5 -7 
+
+print "what is 3-2 ",3-2 
+
+print "is it greater?",5>7
